63fb9a10e1c49452fe9cae31f1b4026c61643560
daniieloo10 <dhiaeddine.boussandel@esprit.tn>
2019-06-22 18:09:48 +0100

    act & part models
    act & part models

---
+++ models/actualites.js
@@ -0,0 +1,25 @@
+
+
+var mongoose = require('mongoose')
+
+var Schema=mongoose.Schema;
+var actualiteSchema = mongoose.Schema({
+
+    title:String,
+    date:Date,
+    description :{
+        type:String,
+        required:false
+    },
+    
+    url :{
+        type:String,
+        required:false
+    },
+    image :{
+        type:String,
+        required:false
+    }
+})
+var actualite =mongoose.model('actualite',actualiteSchema,'actualite');
+module.exports=actualite;
